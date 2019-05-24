162d8e5500d162af985d69730a964ce1490e726d
Iván Martínez <ivanmartinez33@outlook.com>
2019-05-24 11:53:53 -0500

    Se agregó server.js para contectarse a la base de datos

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // Importar dependencias
-const mongo = require('mongodb')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const express = require('express')
 const asyncify = require('express-asyncify')
@@ -14,3 +14,18 @@ if (!process.env.PRODUCTION) {
 // Crear app de express
 const app = asyncify(express())
 
+// Función para iniciar la aplicación
+async function start() {
+    try {
+        // Conectarse a la base de datos
+        mongoose.connect(process.env.DATABASE_URL)
+        // Inicializar aplicación de vue
+        app.listen(process.env.PORT || 3000, () => {
+            console.log('Conexión a la base de datos establecida')
+        })
+    } catch (err) {
+        console.log(`Ocurrió un error al conectarse a la base de datos: ${err}`)
+    }
+}
+
+start()
