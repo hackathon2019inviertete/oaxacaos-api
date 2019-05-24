3dead7f23bd666978e6b5fd73a83d2bce9ac1640
Iván Martínez <ivanmartinez33@outlook.com>
2019-05-24 11:47:38 -0500

    dotenv instalado

--- server.js
+++ server.js
@@ -2,5 +2,15 @@
 
 // Importar dependencias
 const mongo = require('mongodb')
+const dotenv = require('dotenv')
 const express = require('express')
 const asyncify = require('express-asyncify')
+
+// Configurar variables de entorno
+if (!process.env.PRODUCTION) {
+    dotenv.config()
+}
+
+// Crear app de express
+const app = asyncify(express())
+
