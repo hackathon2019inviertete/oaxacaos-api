05384f39087a733ad3982bf5da3c3859052ab174
juxi <juxi34@localhost.localdomain>
2019-05-24 12:42:17 -0500

    modelo reporte

--- models/Administrator.js
+++ models/Administrator.js
@@ -5,11 +5,30 @@ const mongoose = require('mongoose')
 
 //Crear un esquema para el modelo administrador
 const AdminSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  name: String,
+  email:{
+    type: String,
+    require: true
+  },
+
+  password:{
+    type: Number,
+    require: true
+  },
+
+  name: {
+    type: String,
+    require: true
+  },
+
   state: Number
-})
+},
+{
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+}
+)
 
 //exportar el modelo
 const Admin = mongoose.model('admin', AdminSchema)
