bb520e54d47b949f9f28b9002ad87e59ba742f76
Iván Martínez <ivanmartinez33@outlook.com>
2019-05-24 15:21:38 -0500

    semifunción para obtener reportes cera

--- routes/reports.js
+++ routes/reports.js
@@ -44,4 +44,27 @@ router.post('/', jwt(auth.config), async function (req, res, next) {
   }
 })
 
+// Obtener los reportes al rededor de un punto
+// El resultado de los reportes varía si el usuario es administrador
+router.get('/nearby', jwt(auth.config), async function (req, res, next) {
+  // Obtener propiedades
+  const latitude = req.query.latitude
+  const longitude = req.query.longitude
+  const userId = req.user.auth.id
+  const permissions = req.user.permissions
+
+  if (latitude, longitude, userId) {
+    console.log(req.user.permissions)
+    // Revisar si el usuario es normal o admin
+    if (permissions[0] == 'user:normal') {
+      res.send('hola')
+    }
+  } else {
+    // Enviar mensaje de error al usuario
+    res.status(500).json({
+      message: 'No pudimos completar la petición porque faltan datos.'
+    })
+  }
+})
+
 module.exports = router
