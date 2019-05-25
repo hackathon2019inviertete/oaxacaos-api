3a4b8c2e7cb0c0500ed15ce7a1ae0411c9b3292d
Iván Martínez <ivanmartinez33@outlook.com>
2019-05-25 05:46:53 -0500

    yo what the fuck

--- routes/reports.js
+++ routes/reports.js
@@ -18,66 +18,6 @@ const mapsClient = maps.createClient({
   Promise: Promise
 })
 
-// Actualizar el status de un reporte
-// Sólo los administradores pueden acceder a esta ruta
-router.post('/:id/status', jwt(auth.config), guard.check(['user:admin']), async function (req, res, next) {
-  // Obtener propiedades
-  const newStatus = req.body.status
-
-  console.log(newStatus)
-
-  if (newStatus) {
-    try {
-      // Actualizar y regresar reporte
-      const result = await Report.updateStatus(req.params.id, newStatus)
-      res.send(result)
-    } catch (err) {
-      res.next(err)
-    }
-  } else {
-    // Enviar mensaje de error al usuario
-    res.status(500).json({
-      message: 'No pudimos completar la petición porque faltan datos.'
-    })
-  }
-})
-
-// Obtener los reportes al rededor de un punto
-// El resultado de los reportes varía si el usuario es administrador
-router.get('/nearby', jwt(auth.config), async function (req, res, next) {
-  // Obtener propiedades
-  const latitude = req.query.latitude
-  const longitude = req.query.longitude
-  const userId = req.user.auth.id
-  const permissions = req.user.permissions
-
-  if (latitude, longitude, userId) {
-    console.log('******************')
-    console.log(req.user.permissions)
-    console.log(latitude)
-    console.log(longitude)
-    console.log('******************')
-    // Obtener reportes
-    const reports = await Report.findNearby(latitude, longitude)
-    // Obtener denuncias
-    const denuncias = await Denuncia.findNearby(latitude, longitude)
-
-    // Revisar si el usuario es normal o admin
-    if (permissions[0] == 'user:normal') {
-      // Enviar reportes
-      res.send(reports)
-    } else if (permissions[0] == 'user:admin') {
-      // Enviar reportes y denuncias
-      res.send(reports.concat(denuncias))
-    }
-  } else {
-    // Enviar mensaje de error al usuario
-    res.status(500).json({
-      message: 'No pudimos completar la petición porque faltan datos.'
-    })
-  }
-})
-
 // Ruta para crear un nuevo reporte
 // Sólo los usuarios registrados pueden acceder a esta ruta
 router.post('/', jwt(auth.config), async function (req, res, next) {
@@ -137,6 +77,30 @@ router.get('/:id', jwt(auth.config), async function (req, res, next) {
   }
 })
 
+// Actualizar el status de un reporte
+// Sólo los administradores pueden acceder a esta ruta
+router.post('/:id/status', jwt(auth.config), guard.check(['user:admin']), async function (req, res, next) {
+  // Obtener propiedades
+  const newStatus = req.body.status
+
+  console.log(newStatus)
+
+  if (newStatus) {
+    try {
+      // Actualizar y regresar reporte
+      const result = await Report.updateStatus(req.params.id, newStatus)
+      res.send(result)
+    } catch (err) {
+      res.next(err)
+    }
+  } else {
+    // Enviar mensaje de error al usuario
+    res.status(500).json({
+      message: 'No pudimos completar la petición porque faltan datos.'
+    })
+  }
+})
+
 // Actualizar el número de likes
 // Sólo los usuarios pueden acceder a esta ruta
 router.get('/:id/update-likes', jwt(auth.config), guard.check(['user:normal']), async function (req, res, next) {
@@ -150,6 +114,42 @@ router.get('/:id/update-likes', jwt(auth.config), guard.check(['user:normal']), 
     res.send(result)
   } catch (err) {
     next(err)
+  }
+})
+
+// Obtener los reportes al rededor de un punto
+// El resultado de los reportes varía si el usuario es administrador
+router.get('/nearby', jwt(auth.config), async function (req, res, next) {
+  // Obtener propiedades
+  const latitude = req.query.latitude
+  const longitude = req.query.longitude
+  const userId = req.user.auth.id
+  const permissions = req.user.permissions
+
+  if (latitude, longitude, userId) {
+    console.log('******************')
+    console.log(req.user.permissions)
+    console.log(latitude)
+    console.log(longitude)
+    console.log('******************')
+    // Obtener reportes
+    const reports = await Report.findNearby(latitude, longitude)
+    // Obtener denuncias
+    const denuncias = await Denuncia.findNearby(latitude, longitude)
+
+    // Revisar si el usuario es normal o admin
+    if (permissions[0] == 'user:normal') {
+      // Enviar reportes
+      res.send(reports)
+    } else if (permissions[0] == 'user:admin') {
+      // Enviar reportes y denuncias
+      res.send(reports.concat(denuncias))
+    }
+  } else {
+    // Enviar mensaje de error al usuario
+    res.status(500).json({
+      message: 'No pudimos completar la petición porque faltan datos.'
+    })
   }
 })
 
